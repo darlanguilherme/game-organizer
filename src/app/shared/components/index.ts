c8f18335e97d51bd6a837a0905d6f7c8b4716340
Darlan Guilherme <darlanguilherme07@gmail.com>
2020-10-09 19:47:40 -0300

    inicio implementação sidenav

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,3 +2,4 @@ export * from './titlebar/titlebar.component';
 export * from './toolbar/page-not-found.component';
 export * from './titlebar-actions/titlebar-actions.component';
 export * from './sidenav/sidenav.component';
+export * from './input/input.component';
