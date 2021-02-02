ba013d0530a0b831b0a6c975abb0f4bacf36c9ab
Darlan Guilherme <darlanguilherme07@gmail.com>
2021-02-01 22:17:05 -0300

    register-game-component

--- src/app/shared/components/register-game/register-game.component.ts
+++ src/app/shared/components/register-game/register-game.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PoCheckboxGroupOption } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-register-game-found',
@@ -7,9 +9,25 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./register-game.component.scss']
 })
 export class RegisterGameComponent implements OnInit {
+
+  public game: FormGroup = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl(''),
+    description: new FormControl(''),
+    launchDate: new FormControl(''),
+    registerDate: new FormControl(''),
+    tags: new FormControl([]),
+    srcImage: new FormControl(''),
+    idEmulator: new FormControl(''),
+  });
+
   constructor(@Inject(MAT_DIALOG_DATA) public data) {
     console.log("Data! : ", data)
   }
 
   ngOnInit(): void { }
+
+  onSubmit(game) {
+    console.log("GAME: ", game)
+  }
 }
