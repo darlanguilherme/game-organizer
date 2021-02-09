24dee58214eb321c5566f6b92909c93f6679f171
Darlan Guilherme <darlanguilherme07@gmail.com>
2021-02-08 21:28:42 -0300

    alteracoes menu-list component

--- src/app/shared/components/menu-list/menu-list.component.ts
+++ src/app/shared/components/menu-list/menu-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { RegisterGameComponent } from "../../../shared/components/";
 
 @Component({
   selector: 'app-menu-list',
@@ -9,7 +11,29 @@ export class MenuListComponent implements OnInit {
 
   public file = null;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void { }
+
+  public openRegisterGame() {
+    this.openDialog(RegisterGameComponent);
+  }
+
+  public openRegisterEmulator() {
+    this.openDialog(RegisterGameComponent);
+  }
+
+  public openDialog(component) {
+    const dialog = this.dialog.open(component, {
+      data: {
+        animal: 'panda'
+      },
+      panelClass: 'register-game-dialog'
+    });
+
+    dialog.afterClosed().subscribe(result => {
+      console.log("Result aqui: ", result)
+    })
+  }
+
 }
