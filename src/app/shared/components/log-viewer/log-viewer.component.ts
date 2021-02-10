823c50d286c669ce9d62dd53f526cc2961a1eda4
Darlan Guilherme <darlanguilherme07@gmail.com>
2021-02-09 22:16:03 -0300

    criado log-component

---
+++ src/app/shared/components/log-viewer/log-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-log-viewer-found',
+  templateUrl: './log-viewer.component.html',
+  styleUrls: ['./log-viewer.component.scss']
+})
+export class LogViewerComponent implements OnInit {
+
+  constructor() {}
+  ngOnInit(): void { }
+
+  close(){}
+}
